758d44f3109bfca781a26dce36036a299ed7c9eb
Tushar Kumar <archnatushar18@gmail.com>
2024-07-07 21:35:49 +0530

    Test

---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div>
+            <h1 className="text-5xl mb-5 font-light ">DoctorDate.</h1>
+          </div>
+          {/* <div ><h1 className="text-5xl mb-5 font-light ">DoctorDate.</h1></div> */}
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 DoctorDate
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+};
+
+export default Home;
